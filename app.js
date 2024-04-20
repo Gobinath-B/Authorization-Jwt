55314b1de0fc698138a456465f25cf5d8ab637ac
Gobinath B <princegopinath42@gmail.com>
2024-04-20 14:05:02 +0530

    refactor

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const app = express();
 const port = 5000;
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
+
+app.use(cors());
 app.use(express.json());
 
-const connectionUrl = "mongodb://127.0.0.1:27017/user"
+const connectionUrl = `mongodb+srv://${process.env.MONGOO_USERNAME}:${process.env.MONGO_PASSWORD}@userdata.e6nklgz.mongodb.net/?retryWrites=true&w=majority&appName=UserData`
 mongoose.connect(connectionUrl)
     .then(() => {
         console.log("MongoDB connected successfully");
@@ -21,45 +24,37 @@ mongoose.connect(connectionUrl)
         console.error("MongoDB connection error:", error);
     });
 
-// const users = [
-//     {
-//         username : "itachi",
-//         aka : "clanSlayer"
-//     },
-//     {
-//         username : "minato_namikazae",
-//         clan : "yellowFlash"
-//     }
 
-// ]
+    const userSchema = new mongoose.Schema({
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/\S+@\S+\.\S+/, 'Please use a valid email address.'], 
+            lowercase: true 
+        },
+        contactNumber: {
+            type: String,
+            required: false, 
+            match: [/^\d{10}$/, 'Please use a valid contact number.'], 
+        }
+    });
 
-const userSchema =new mongoose.Schema({
-   
-    username: {type:String,required:true,unique:true},
-    password:{type:String,required:true},
-});
 
-//model
 
 const User =  mongoose.model("User",userSchema);
 
 
-
-// app.get('/post',Authenticate,(req,res)=>{
-// //   res.json(users.filter(user => user.username == req.user.name));
-// })
-
-// app.post('/login',(req,res)=>{
-
-//     console.log(req.body);
-//     const username = req.body.username;
-//     const user = {name:username};
-
-//    const accessToken =  jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
-//    res.json({accessToken:accessToken});
-// })
-
-app.get('/post', Authenticate, async (req, res) => {
+app.get('/login', Authenticate, async (req, res) => {
     try {
         console.log("user",req.user);
         const user = await User.findOne({ username: req.user.username });
@@ -99,7 +94,8 @@ app.post("/signup",async(req,res)=>{
      const username = req.body.username;
      const user = {username:username};
  
-     const accessToken =  jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
+     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
      res.cookie('jwt', accessToken, {
         httpOnly: true, 
         secure: true, 
